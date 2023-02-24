c96286930516cc6e36810e4337f85fba7df151c7
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-02-24 20:17:47 +0700

    in development

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import telebot
 import logging
 from dotenv import load_dotenv
 from telebot import types
+from openpyxl import load_workbook
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -12,12 +13,39 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
 
+# FUNCS FOR BOT
+
+
+def parse_excel():
+    book = load_workbook("workbook")
+    sheet = book.active
+
+    rows = sheet.rows
+
+    headers = [cell.value for cell in next(rows)]
+
+    all_rows = []
+
+    for row in rows:
+        data = {}
+        for title, cell in zip(headers, row):
+            data[title] = cell.value
+
+        all_rows.append(data)
+
+    return all_rows
+
+
+# BOT LOGIC
+
+
 @bot.message_handler(commands=['start'])
 def start(msg):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     api_btn = types.KeyboardButton("Дока API")
     github_btn = types.KeyboardButton("Github репозиторий")
-    markup.add(api_btn, github_btn)
+    excel_btn = types.KeyboardButton("Достать почту из Excel файла")
+    markup.add(api_btn, github_btn, excel_btn)
     bot.send_message(msg.chat.id, text="Привет, {0.first_name}! Я тестовый бот!".format(msg.from_user),
                      reply_markup=markup)
 
@@ -36,6 +64,30 @@ def reply(msg):
         markup.add(btn_github)
         bot.send_message(msg.chat.id, "Привет!!!, {0.first_name}! Нажми на кнопку и перейди на сайт".format(msg.from_user),
                          reply_markup=markup)
+    elif msg.text == "Достать почту из Excel файла":
+        markup = types.InlineKeyboardMarkup()
+        bot.send_message(msg.chat.id, "Просто закинь Excel файл".format(msg.from_user),
+                         reply_markup=markup)
+
+
+@bot.message_handler(content_types=['document'])
+def handle_docs(msg):
+    try:
+        chat_id = msg.chat.id
+
+        file_info = bot.get_file(msg.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        src = '' + msg.document.file_name
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        bot.reply_to(msg, "Пожалуй, я сохраню это")
+    except Exception as e:
+        bot.reply_to(msg, e)
+
+    all_rows = parse_excel()
+    print(all_rows)
 
 
 bot.infinity_polling()
