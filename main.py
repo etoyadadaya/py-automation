5082c19805d65926e87c2453e60c1da9cc5c03eb
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-02-25 13:59:31 +0700

    excel logic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import telebot
 import logging
+import pandas
 from dotenv import load_dotenv
 from telebot import types
-from openpyxl import load_workbook
+
+
+# VARIABLES INITIALIZE
+
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
@@ -15,25 +19,22 @@ telebot.logger.setLevel(logging.DEBUG)
 
 # FUNCS FOR BOT
 
-
 def parse_excel():
-    book = load_workbook("workbook")
-    sheet = book.active
+    usernames = []
+    excel_data = pandas.read_excel('Recipients data.xlsx', sheet_name='Успеваемость')
 
-    rows = sheet.rows
+    for column in excel_data['Логин'].tolist():
+        usernames.append(column)
 
-    headers = [cell.value for cell in next(rows)]
+    with open("usernames.txt", "w") as file:
+        for user in usernames:
+            file.write(f"{user}\n")
 
-    all_rows = []
 
-    for row in rows:
-        data = {}
-        for title, cell in zip(headers, row):
-            data[title] = cell.value
-
-        all_rows.append(data)
-
-    return all_rows
+def send_docs(msg):
+    doc = open("usernames.txt", "rb")
+    bot.send_document(msg.chat.id, doc)
+    os.remove("usernames.txt")
 
 
 # BOT LOGIC
@@ -42,31 +43,31 @@ def parse_excel():
 @bot.message_handler(commands=['start'])
 def start(msg):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    api_btn = types.KeyboardButton("Дока API")
-    github_btn = types.KeyboardButton("Github репозиторий")
-    excel_btn = types.KeyboardButton("Достать почту из Excel файла")
+    api_btn = types.KeyboardButton("API docs")
+    github_btn = types.KeyboardButton("Github repository")
+    excel_btn = types.KeyboardButton("Extract email from Excel")
     markup.add(api_btn, github_btn, excel_btn)
-    bot.send_message(msg.chat.id, text="Привет, {0.first_name}! Я тестовый бот!".format(msg.from_user),
+    bot.send_message(msg.chat.id, text="Hi, {0.first_name}! Im BOT!".format(msg.from_user),
                      reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def reply(msg):
-    if msg.text == "Дока API":
+    if msg.text == "API docs":
         markup = types.InlineKeyboardMarkup()
-        btn_api = types.InlineKeyboardButton("Дока API", url="https://pytba.readthedocs.io/ru/latest/index.html")
+        btn_api = types.InlineKeyboardButton("API docs", url="https://pytba.readthedocs.io/ru/latest/index.html")
         markup.add(btn_api)
-        bot.send_message(msg.chat.id, "Привет, {0.first_name}! Нажми на кнопку и перейди на сайт".format(msg.from_user),
+        bot.send_message(msg.chat.id, "Hello, {0.first_name}! Your link is ready :)".format(msg.from_user),
                          reply_markup=markup)
-    elif msg.text == "Github репозиторий":
+    elif msg.text == "Github repository":
         markup = types.InlineKeyboardMarkup()
-        btn_github = types.InlineKeyboardButton("Ссылка на GitHub", url="https://github.com/etoyadadaya/py-automation")
+        btn_github = types.InlineKeyboardButton("Github repository", url="https://github.com/etoyadadaya/py-automation")
         markup.add(btn_github)
-        bot.send_message(msg.chat.id, "Привет!!!, {0.first_name}! Нажми на кнопку и перейди на сайт".format(msg.from_user),
+        bot.send_message(msg.chat.id, "Hello, {0.first_name}! Your link is ready :)".format(msg.from_user),
                          reply_markup=markup)
-    elif msg.text == "Достать почту из Excel файла":
+    elif msg.text == "Extract email from Excel":
         markup = types.InlineKeyboardMarkup()
-        bot.send_message(msg.chat.id, "Просто закинь Excel файл".format(msg.from_user),
+        bot.send_message(msg.chat.id, "Input Excel-file".format(msg.from_user),
                          reply_markup=markup)
 
 
@@ -82,12 +83,12 @@ def handle_docs(msg):
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
 
-        bot.reply_to(msg, "Пожалуй, я сохраню это")
+        bot.reply_to(msg, "saved!")
+        parse_excel()
+        os.remove("Recipients data.xlsx")
+        send_docs(msg)
     except Exception as e:
         bot.reply_to(msg, e)
-
-    all_rows = parse_excel()
-    print(all_rows)
 
 
 bot.infinity_polling()
